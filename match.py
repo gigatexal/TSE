c573138326e6c237d0ff3c97bba68d3dc8ee640d
Alex N <alexandar.narayan@gmail.com>
2015-05-30 11:41:56 -0700

    attempt1

---
+++ match.py
@@ -0,0 +1,2 @@
+print ('create a function match() that takes two arguments, and doesn''t use any string functions other than one that finds the length, and another that returns the length of the string')
+
