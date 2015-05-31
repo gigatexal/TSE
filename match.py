8be640316d1bd7748896084e7f0cbb8a2d7aa298
Alex N <alexandar.narayan@gmail.com>
2015-05-30 21:10:25 -0700

    done

--- match.py
+++ match.py
@@ -1,2 +1,17 @@
-print ('create a function match() that takes two arguments, and doesn''t use any string functions other than one that finds the length, and another that returns the length of the string')
+strToSearch = "This is a test string"
+pattern = "is"
+
+#create a function called match that takes two arguments, one the pattern, the other the string, returns true or false if the pattern is found or not found.
+
+lenStr = len(strToSearch)
+lenPattern = len(pattern)
+
+for i in xrange(0,lenStr):
+	if (!pattern[0] == strToSearch[i]):
+		break;
+		print ("false")
+	elif (
+
+	else:
+		print ("not found")	
 
